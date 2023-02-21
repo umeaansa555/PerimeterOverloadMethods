61259b438b2056f3fbce84f19fd22d3990bfe15f
Aimen Ansari <umeaansa555@ed.amdsb.ca>
2023-02-21 10:04:41 -0500

    Add project files.

---
+++ OverloadMethods/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace OverloadMethods
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.areaOutput = new System.Windows.Forms.Label();
+            this.value2Label = new System.Windows.Forms.Label();
+            this.value1Label = new System.Windows.Forms.Label();
+            this.value2Input = new System.Windows.Forms.TextBox();
+            this.value1Input = new System.Windows.Forms.TextBox();
+            this.areaButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.value3Label = new System.Windows.Forms.Label();
+            this.value3Input = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // areaOutput
+            // 
+            this.areaOutput.BackColor = System.Drawing.Color.White;
+            this.areaOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.areaOutput.Location = new System.Drawing.Point(55, 413);
+            this.areaOutput.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.areaOutput.Name = "areaOutput";
+            this.areaOutput.Size = new System.Drawing.Size(379, 76);
+            this.areaOutput.TabIndex = 19;
+            this.areaOutput.Text = "...";
+            // 
+            // value2Label
+            // 
+            this.value2Label.AutoSize = true;
+            this.value2Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value2Label.Location = new System.Drawing.Point(55, 302);
+            this.value2Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.value2Label.Name = "value2Label";
+            this.value2Label.Size = new System.Drawing.Size(105, 31);
+            this.value2Label.TabIndex = 18;
+            this.value2Label.Text = "Value 2";
+            // 
+            // value1Label
+            // 
+            this.value1Label.AutoSize = true;
+            this.value1Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value1Label.Location = new System.Drawing.Point(55, 250);
+            this.value1Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.value1Label.Name = "value1Label";
+            this.value1Label.Size = new System.Drawing.Size(105, 31);
+            this.value1Label.TabIndex = 17;
+            this.value1Label.Text = "Value 1";
+            // 
+            // value2Input
+            // 
+            this.value2Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value2Input.Location = new System.Drawing.Point(175, 298);
+            this.value2Input.Margin = new System.Windows.Forms.Padding(4);
+            this.value2Input.Name = "value2Input";
+            this.value2Input.Size = new System.Drawing.Size(83, 37);
+            this.value2Input.TabIndex = 16;
+            // 
+            // value1Input
+            // 
+            this.value1Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value1Input.Location = new System.Drawing.Point(175, 246);
+            this.value1Input.Margin = new System.Windows.Forms.Padding(4);
+            this.value1Input.Name = "value1Input";
+            this.value1Input.Size = new System.Drawing.Size(83, 37);
+            this.value1Input.TabIndex = 15;
+            // 
+            // areaButton
+            // 
+            this.areaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.areaButton.Location = new System.Drawing.Point(267, 246);
+            this.areaButton.Margin = new System.Windows.Forms.Padding(4);
+            this.areaButton.Name = "areaButton";
+            this.areaButton.Size = new System.Drawing.Size(167, 143);
+            this.areaButton.TabIndex = 14;
+            this.areaButton.Text = "Area";
+            this.areaButton.UseVisualStyleBackColor = true;
+            this.areaButton.Click += new System.EventHandler(this.areaButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(34, 40);
+            this.titleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(421, 52);
+            this.titleLabel.TabIndex = 20;
+            this.titleLabel.Text = "Perimeter Calculator";
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.AutoSize = true;
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(121, 102);
+            this.instructionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(148, 25);
+            this.instructionLabel.TabIndex = 21;
+            this.instructionLabel.Text = "instructionLabel";
+            // 
+            // value3Label
+            // 
+            this.value3Label.AutoSize = true;
+            this.value3Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value3Label.Location = new System.Drawing.Point(55, 356);
+            this.value3Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.value3Label.Name = "value3Label";
+            this.value3Label.Size = new System.Drawing.Size(105, 31);
+            this.value3Label.TabIndex = 23;
+            this.value3Label.Text = "Value 3";
+            // 
+            // value3Input
+            // 
+            this.value3Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.value3Input.Location = new System.Drawing.Point(175, 352);
+            this.value3Input.Margin = new System.Windows.Forms.Padding(4);
+            this.value3Input.Name = "value3Input";
+            this.value3Input.Size = new System.Drawing.Size(83, 37);
+            this.value3Input.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.ClientSize = new System.Drawing.Size(492, 556);
+            this.Controls.Add(this.value3Label);
+            this.Controls.Add(this.value3Input);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.areaOutput);
+            this.Controls.Add(this.value2Label);
+            this.Controls.Add(this.value1Label);
+            this.Controls.Add(this.value2Input);
+            this.Controls.Add(this.value1Input);
+            this.Controls.Add(this.areaButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Area Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label areaOutput;
+        private System.Windows.Forms.Label value2Label;
+        private System.Windows.Forms.Label value1Label;
+        private System.Windows.Forms.TextBox value2Input;
+        private System.Windows.Forms.TextBox value1Input;
+        private System.Windows.Forms.Button areaButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Label value3Label;
+        private System.Windows.Forms.TextBox value3Input;
+    }
+}
+
---
+++ OverloadMethods/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OverloadMethods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            instructionLabel.Text = "Enter: \n\n1 value for a circle\n2 values for a rectangle";
+        }
+
+        private void areaButton_Click(object sender, EventArgs e)
+        {
+            //double num1, num2, answer;
+            // declare variables at the top for clarity/legibility
+            double num1, num2, num3, answer;
+
+            if (value2Input.Text == "" && value3Input.Text == "")
+            {
+                num1 = Convert.ToDouble(value1Input.Text);
+                answer = Perimeter(num1);
+                areaOutput.Text = $"The perimeter of the circle is {answer}";
+            }
+
+            else if (value3Input.Text == "")
+            {
+                num1 = Convert.ToDouble(value1Input.Text);
+                num2 = Convert.ToDouble(value2Input.Text);
+                answer = Perimeter(num1, num2);
+                areaOutput.Text = $"The perimeter of the rectangle is {answer}";
+            }
+            else if (value1Input.Text == "" && value2Input.Text == "" && value3Input.Text == "")
+            {
+                areaOutput.Text = "Please enter at least 1 value";
+            }
+            else
+            {
+                num1 = Convert.ToDouble(value1Input.Text);
+                num2 = Convert.ToDouble(value2Input.Text);
+                num3 = Convert.ToDouble(value3Input.Text);
+                answer = Perimeter(num1, num2, num3);
+                areaOutput.Text = $"The perimeter of the triangle is {answer}";
+            }
+        }
+
+        public double Perimeter(double diameter)
+        {
+            // double area = 3.14 * radius * radius;
+            // OR the shorter way: using the Math class of built in calculation stuff
+
+            //double area = Math.PI * Math.Pow(radius, 2);
+            //return area;
+            double p = 3.14 * diameter;
+            return p;
+
+        }
+        public double Perimeter(double length, double width)
+        {
+            //double area = length * width;
+            //return area;
+            double p = (2 * length) * (2 * width);
+            return p;
+
+        }
+        public double Perimeter(double a, double b, double c)
+        {
+            double p = a + b + c;
+            return p;
+            
+        }
+    }
+}
